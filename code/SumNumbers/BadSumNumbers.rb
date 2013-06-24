889c4b02ae347300eb63fafe8a8720d903b69629
Dmitry Ilyin <dilyin@mirantis.com>
2013-06-24 12:49:41 +0400

    Finish rspec remake

--- code/SumNumbers/BadSumNumbers.rb
+++ code/SumNumbers/BadSumNumbers.rb
@@ -1,8 +1,10 @@
-# SumNumbers
+# BadSumNumbers
 #
 # Простой класс, демонстрирующий тестирование при помощи Rspec.
+# Содержит ошибку в реализации вычитания для демонстрации
+# провала теста.
 #
-class SumNumbers
+class BadSumNumbers
   attr_accessor :sum
   def initialize(start_sum = 0)
     @sum = start_sum
@@ -11,7 +13,7 @@ class SumNumbers
     @sum += add_num
   end
   def sub(sub_num = 0)
-    @sum -= sub_num
+    @sum += sub_num
   end
   def positive?
     @sum > 0
--- code/SumNumbers/spec/BadSumNumbers_spec.rb
+++ code/SumNumbers/spec/BadSumNumbers_spec.rb
@@ -1,36 +1,36 @@
 require 'spec_helper'
-require './SumNumbers'
+require './BadSumNumbers'
 
-describe SumNumbers do
+describe BadSumNumbers do
   it "should have zero sum to begin with" do
-    sn = SumNumbers.new
+    sn = BadSumNumbers.new
     sn.sum.should == 0
   end
   it "should have given initial sum" do
-    sn = SumNumbers.new(3)
+    sn = BadSumNumbers.new(3)
     sn.sum.should == 3
   end
   it "should add every new number to the sum" do
-    sn = SumNumbers.new
+    sn = BadSumNumbers.new
     sn.add(5)
     sn.sum.should == 5
   end
   it "should add all new numbers to initial sum" do
-    sn = SumNumbers.new(2)
+    sn = BadSumNumbers.new(2)
     sn.add(2)
     sn.sum.should == 4
   end
   it "should substract given value" do
-    sn = SumNumbers.new(5)
+    sn = BadSumNumbers.new(5)
     sn.sub(3)
     sn.sum.should == 2
   end
   it "should be positive if sum is more ther zero" do
-    sn = SumNumbers.new(1) 
+    sn = BadSumNumbers.new(1) 
     sn.should be_positive
   end
   it "should not be positive if sum is lesser or equal zero" do
-    sn = SumNumbers.new(-1) 
+    sn = BadSumNumbers.new(-1) 
     sn.should_not be_positive
   end
 end
